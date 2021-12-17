f7f7b2d9184ef6a5adeceb92417d160c481267bf
César O. Araújo <oaraujocesar@gmail.com>
2021-12-17 19:42:33 -0300

    style: :lipstick: Create first styles for Main

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  moduleNameMapper: {
+    '^styled-components':
+      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+  }
 }
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import { Title, Wrapper, Description, Logo, Illustration } from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <Wrapper>
+    <Logo
+      src="/images/logo.svg"
+      alt="Imagem de um átomo rosa e 'React Avançado' escrito ao lado."
+    />
+    <Title>React Avançado</Title>
+    <Description>TypeScript, ReactJS, NextJS e Styled Components</Description>
+    <Illustration
+      src="/images/hero-illustration.svg"
+      alt="Ilustração de um desenvolvedor sentado de frente para uma tela com código."
+    />
+  </Wrapper>
 )
 
 export default Main
