2e6f9747888ee978c7839ca9a66bd1713f9e36d9
César O. Araújo <oaraujocesar@gmail.com>
2021-12-17 21:45:44 -0300

    chore: :heavy_plus_sign: Set up Next PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line no-undef
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  },
   reactStrictMode: true
-}
+})
